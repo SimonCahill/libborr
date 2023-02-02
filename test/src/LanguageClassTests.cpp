5e67d427b533b81cd33437cd0565f4b1fa0bd202
Simon Cahill <simon@simonc.eu>
2023-02-02 23:40:43 +0100

    Added test cases

--- test/src/LanguageClassTests.cpp
+++ test/src/LanguageClassTests.cpp
@@ -12,11 +12,40 @@
 
 #include <gtest/gtest.h>
 
-class LanguageClassTests: public testing::Test, borr::language {
+using std::string;
+
+class LanguageClassTests: public testing::Test, public borr::language {
     protected:
         void SetUp() {
-            m_language = borr::language();
         }
-
-        borr::language m_language;
 };
+
+TEST_F(LanguageClassTests, testIsEmptyOrComment) {
+    const static string COMMENT_LINE    = "#this line is a comment";
+    const static string COMMENT_LINE_2  = "# this line is also a comment!";
+    const static string COMMENT_LINE_3  = "  # this is another comment line";
+    const static string EMPTY_LINE      = "";
+    const static string EMPTY_LINE_2    = "       ";
+    const static string EMPTY_LINE_3    = "\t\t\t";
+
+    ASSERT_TRUE(isEmptyOrComment(COMMENT_LINE));
+    ASSERT_TRUE(isEmptyOrComment(COMMENT_LINE_2));
+    ASSERT_TRUE(isEmptyOrComment(COMMENT_LINE_3));
+    ASSERT_TRUE(isEmptyOrComment(EMPTY_LINE));
+    ASSERT_TRUE(isEmptyOrComment(EMPTY_LINE_2));
+    ASSERT_TRUE(isEmptyOrComment(EMPTY_LINE_3));
+
+    ASSERT_FALSE(isEmptyOrComment("This is neither a command, not an empty line!"));
+}
+
+TEST_F(LanguageClassTests, testRemoveInlineComments) {
+    const static string SECTION_WITH_COMMENT = R"([section] # and a comment)";
+    const static string SECTION_WITH_COMMENT_2 = R"([section]#comment)";
+    const static string TRANSLATION_WITH_COMMENT = R"(translation = "" # comment)";
+    const static string TRANSLATION_WITH_COMMENT_2 = R"(translation[] = ""#COMMENTS ARE WEIRD)";
+
+    ASSERT_EQ(removeInlineComments(SECTION_WITH_COMMENT), R"([section])");
+    ASSERT_EQ(removeInlineComments(SECTION_WITH_COMMENT_2), R"([section])");
+    ASSERT_EQ(removeInlineComments(TRANSLATION_WITH_COMMENT), R"(translation = "")");
+    ASSERT_EQ(removeInlineComments(TRANSLATION_WITH_COMMENT_2), R"(translation[] = "")");
+}
