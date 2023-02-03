d91e857b30adb9b5c9adccef5c3cce5cfe9f998c
Simon Cahill <simon@simonc.eu>
2023-02-04 00:24:21 +0100

    Added known-good string splitter

--- include/borr/extensions.hpp
+++ include/borr/extensions.hpp
@@ -21,6 +21,36 @@ namespace borr::extensions {
     using std::string;
     using std::vector;
 
+
+    /**
+     * @brief Splits a string by the givn delimiters.
+     * 
+     * @param str The string to split.
+     * @param delimiters The delimiters to split the string by.
+     * @param outTokens The output tokens.
+     * @param maxLen The maximum amount of tokens.
+     * 
+     * @return true If splitting was successful.
+     * @return false Otherwise.
+     */
+    inline bool splitString(const string& str, const string& delimiters, vector<string>& outTokens, size_t maxLen = SIZE_MAX) {
+        // Skip delimiters at beginning.
+        size_t lastPos = str.find_first_not_of(delimiters, 0);
+        // Find first "non-delimiter".
+        size_t pos = str.find_first_of(delimiters, lastPos);
+
+        while ((string::npos != pos || string::npos != lastPos) && maxLen < outTokens.size()) {
+            // Found a token, add it to the vector.
+            outTokens.push_back(str.substr(lastPos, pos - lastPos));
+            // Skip delimiters.
+            lastPos = string::npos == pos ? string::npos : pos + 1;
+            // Find next "non-delimiter"
+            pos = str.find_first_of(delimiters, lastPos);
+        }
+
+        return outTokens.size() > 0;
+    }
+
     /**
      * @brief Trims the beginning of a given string.
      *
@@ -30,11 +60,9 @@ namespace borr::extensions {
      * @return The trimmed string.
      */
     inline string trimStart(string nonTrimmed, const string& trimChar = " \t\r") {
-        //nonTrimmed.erase(nonTrimmed.begin(), find_if(nonTrimmed.begin(), nonTrimmed.end(), not1(ptr_fun<int32_t, int32_t>(isspace))));
-
         function<bool(char)> shouldTrimChar = [=](char c) -> bool { return trimChar.size() == 0 ? isspace(c) : trimChar.find(c) != string::npos; };
 
-        nonTrimmed.erase(nonTrimmed.begin(), find_if(nonTrimmed.begin(), nonTrimmed.end(), not1(shouldTrimChar)));
+        nonTrimmed.erase(nonTrimmed.begin(), find_if(nonTrimmed.begin(), nonTrimmed.end(), std::not_fn(shouldTrimChar)));
 
         return nonTrimmed;
     }
@@ -47,10 +75,8 @@ namespace borr::extensions {
      * @return The trimmed string.
      */
     inline string trimEnd(string nonTrimmed, const string& trimChar = " \t\r") {
-        // nonTrimmed.erase(find_if(nonTrimmed.rbegin(), nonTrimmed.rend(), not1(ptr_fun<int32_t, int32_t>(isspace))).base(), nonTrimmed.end());
-
         function<bool(char)> shouldTrimChar = [=](char c) -> bool { return trimChar.size() == 0 ? isspace(c) : trimChar.find(c) != string::npos; };
-        nonTrimmed.erase(find_if(nonTrimmed.rbegin(), nonTrimmed.rend(), not1(shouldTrimChar)).base(), nonTrimmed.end());
+        nonTrimmed.erase(find_if(nonTrimmed.rbegin(), nonTrimmed.rend(), std::not_fn(shouldTrimChar)).base(), nonTrimmed.end());
 
         return nonTrimmed;
     }
