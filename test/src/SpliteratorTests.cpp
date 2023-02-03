996a7df77dbfdcbfaad024788548616f14758ad8
Simon Cahill <simon@simonc.eu>
2023-02-04 00:26:10 +0100

    Added initial spliterator tests

---
+++ test/src/SpliteratorTests.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file SpliteratorTests.cpp
+ * @author Simon Cahill (contact@simonc.eu)
+ * @brief Contains unit tests for the spliterator
+ * @version 0.1
+ * @date 2023-02-03
+ * 
+ * @copyright Copyright (c) 2023 Simon Cahill and Contributors.
+ */
+
+#include "borr/string_splitter.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(SpliteratorTests, testSpaceSplit) {
+    
+}
