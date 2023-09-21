a691e4c82aec781951391e89bb44e94adb6f52f0
Simon Cahill <simon@simonc.eu>
2023-09-21 19:52:25 +0200

    Added check for pre-existing fmt target

--- cmake/format_support.cmake
+++ cmake/format_support.cmake
@@ -14,13 +14,16 @@ if (DEFINED CXX_FORMAT_SUPPORT AND "${CXX_FORMAT_SUPPORT}" STREQUAL "1")
     message("std::format found! Lucky you!")
     set(FMT_LIB_NAME "")
 else()
-    include(FetchContent)
-    FetchContent_Declare(
-        fmt-header-only
-        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-        GIT_TAG f5e54359df4c26b6230fc61d38aa294581393084
-    )
 
-    FetchContent_MakeAvailable(fmt-header-only)
+    if (NOT TARGET fmt-header-only)
+        include(FetchContent)
+        FetchContent_Declare(
+            fmt-header-only
+            GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+            GIT_TAG f5e54359df4c26b6230fc61d38aa294581393084
+        )
+        FetchContent_MakeAvailable(fmt-header-only)
+    endif()
+
     set(FMT_LIB_NAME "fmt::fmt-header-only")
 endif()
